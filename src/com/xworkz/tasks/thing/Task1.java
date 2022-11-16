c3792dd4831cb59f515a88412d706e2a8b463ab6
Gagansv <gagansv.xworkz@gmail.com>
2022-11-16 20:10:12 +0530

    reverse sentence

---
+++ src/com/xworkz/tasks/thing/Task1.java
@@ -0,0 +1,15 @@
+package com.xworkz.tasks.thing;
+
+public class Task1 {
+	public static class ReverseWords {
+	    public static void main(String args[]) {
+	      String s[] = "Xworkz is a training institute".split(" "); 
+	      String ans = ""; 
+	      for (int i = s.length - 1; i >= 0; i--) { 
+	        ans = ans+ s[i] + " "; 
+	      } 
+	      System.out.println("Reversed String: " + ans); 
+	    }
+	}
+	
+}
