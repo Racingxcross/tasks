21a2893884ea72743e4475bcc95b89512a1f38ca
Gagansv <gagansv.xworkz@gmail.com>
2022-11-24 23:23:56 +0530

    task

---
+++ src/com/xworkz/tasks/thing/AlphabetPattern.java
@@ -0,0 +1,23 @@
+package com.xworkz.tasks.thing;
+
+public class AlphabetPattern {
+
+	public static void main(String[] args) {
+	
+	for(int i=1;i<=1;i++) {
+		
+		for (int j=2; j<=2; j++) {
+			
+		for (int k=3; k<=3; k++) {
+			
+			System.out.println("a");
+		}
+		System.out.println("bb");
+	}
+	System.out.println("ccc");
+	}
+  }
+}
+
+
+//write aprogram toprint thebelow output input:->abcd output:->A-Bb-Ccc-Dddd
